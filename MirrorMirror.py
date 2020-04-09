a48a51f5e228cf92e152e4a4271f753133e04074
jool <noobjool@gmail.com>
2020-04-09 14:41:44 +0200

    Skapat egen @route i Python för widget:kalender  i MirrorMirror.py. Skapat månads-kalender i Python som hämtar aktuell tid och datum med svenska veckodagar. Flyttat över kalenderdatan från JSON-filen calender-data.json till Python och sen raderat JSON-filen som ej längre behövs. Lagt till import response i Python för att berätta för webbläsaren att JSON används. Uppdaterat calender.js med rätt sökväg. Lagt till förklarande kommentarer i koden och referens.

--- MirrorMirror.py
+++ MirrorMirror.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template, static_file
+from bottle import route, run, template, static_file, response
 import psycopg2
 
 
@@ -10,6 +10,55 @@ def Home_page():
 @route("/static/<filename>")
 def static_files(filename):
     return static_file(filename, root="static")
+
+
+@route("/api/calendar")
+def cal():
+# Creates a monthly calendar with current date and local weekday names
+# Provides calendar data with the help of JSON
+# Reference: https://docs.python.org/3/library/calendar.html
+
+    import calendar
+    import json
+    from datetime import date
+
+    today = date.today()
+    year = int(today.strftime("%Y"))
+    month = int(today.strftime("%m"))
+
+    c = calendar.LocaleHTMLCalendar(calendar.MONDAY, "sv_SE")
+
+    cal = {
+        "calendarHTML": c.formatmonth(year, month),
+        "entries": [
+                {
+                "title": "Klippa mig",
+                "when": "Idag",
+                "time": "13:50-14:20",
+                "day": "8"
+            },
+            {
+                "title": "Klippa hunden",
+                "when": "Imorgon",
+                "time": "13:50-14:20",
+                "day": "9"
+            },
+            {
+                "title": "Klippa min kompis",
+                "when": "I övermorgon",
+                "time": "13:50-14:20",
+                "day": "10"
+            },
+            {
+                "title": "Klippa någon annan",
+                "when": "I övermorgon",
+                "time": "14:50-15:20",
+                "day": "15"
+            }
+        ]
+    }
+    response.content_type = 'application/json'
+    return json.dumps(cal) 
 
  
 run(host='localhost', port=8080)
