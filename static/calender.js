458585e4d22f7dbd9685bff6e4fa13ef230ee1c8
jool <noobjool@gmail.com>
2020-04-29 13:38:49 +0200

    Kört igenom kod med https://beautifier.io och sen ändrat indentering

--- static/calender.js
+++ static/calender.js
@@ -1,13 +1,12 @@
 // Fetches monthly calendar and calendar data från MirrorMirror.py with JSON.
 // Calendar data output is sorted by day, title, when and time.
-
 fetch("/api/calendar")
-  .then(response => response.json())
-  .then((json) => {
-      var output = json.calendarHTML;
-      json.entries.forEach(elem => {
-          output += `${elem.day} ${elem.title} <br> ${elem.when} ${elem.time} <br>`;
-      })
-      var cal = document.querySelector('.CalenderWidget');
-          cal.innerHTML = output
-  });
+    .then(response => response.json())
+    .then((json) => {
+        var output = json.calendarHTML;
+        json.entries.forEach(elem => {
+            output += `${elem.day} ${elem.title} <br> ${elem.when} ${elem.time} <br>`;
+        })
+        var cal = document.querySelector('.CalenderWidget');
+        cal.innerHTML = output
+    });
