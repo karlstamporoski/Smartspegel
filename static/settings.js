da2aa76a6cfe4307056a65a1ba11fd9cd6cb099e
Azii4 <62041963+Azii4@users.noreply.github.com>
2020-05-10 10:47:49 +0200

    border for active drag
    added dotted border for widgets with active Draggable class

--- static/settings.js
+++ static/settings.js
@@ -14,3 +14,16 @@ show.addEventListener('click', () => {
 
   $(".Up, .Down").toggleClass("Up Down");
 });
+
+
+
+const lock = document.querySelector(".LockDrag");
+const drag = document.querySelector(".Draggable");
+const TimeDateWrapper = document.querySelector(".TimeDateWrapper");
+const CalendarWrapper = document.querySelector("CalendarWrapper");
+const NewsWrapper = document.querySelector(".NewsWrapper");
+const WeatherWrapper = document.querySelector(".WeatherWrapper");
+lock.addEventListener('click', () => {
+    
+    $(".TimeDateWrapper, .CalendarWrapper, .NewsWrapper, .WeatherWrapper").toggleClass("Draggable")
+})
