96fc1764caed60284c037c1791608dd8b0535fcc
Alvaroer412 <62143922+Alvaroer412@users.noreply.github.com>
2020-05-13 14:34:39 +0200

    uppdaterat javascript
    Co-Authored-By: Mohamud <k-tech32@users.noreply.github.com>

--- static/weather.js
+++ static/weather.js
@@ -1,97 +1,45 @@
-// Väljer element
-const iconElement = document.querySelector(".weather-icon");
-const tempElement = document.querySelector(".temperature-value p");
-const descElement = document.querySelector(".temperature-description p");
-const locationElement = document.querySelector(".location p");
-const notificationElement = document.querySelector(".notification");
+const WEATHER_API_KEY = "cff11be67c426dc8ddb0c16b7561b3c1";
+const WEATHER_URL = "http://api.openweathermap.org/data/2.5/forecast?";
 
-// Definierar weather data
-const weather = {};
-
-weather.temperature = {
-    unit: "celsius"
-}
-
-// Definierar KELVIN
-const KELVIN = 273;
-
-// API nyckel
-const key = "82005d27a116c2880c8f0fcb866998a0";
-
-// Kollar om webbläsaren stöder geolocation.
-if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(setPosition, showError);
-} else {
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
-}
-
-// Tar reda på användarens position.
-function setPosition(position) {
+function getCoordinates(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-
-    getWeather(latitude, longitude);
+    
+    fetchWeatherData(latitude, longitude);
 }
 
-// Om geolocation inte fungerar får man ett felmeddelande.
-function showError(error) {
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+function error() {
+    console.log("Unable to fetch your position");
 }
 
+function displayWeather(weatherData) {
+    console.log("Recieve this weather data");
+    console.log(weatherData);
+    console.log(weatherData.city.name);
+}
 
+function fetchWeatherData(latitude, longitude) {
+    const url = `${WEATHER_URL}lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`;
 
-/*
-// Får vädret från API:n. 
-function getWeather(latitude, longitude) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-
-    fetch(api)
-        .then(function (response) {
-            let data = response.json();
-            return data;
+    fetch(url)
+        .then(function(response) {
+            const weatherData = response.json();
+            return weatherData;
         })
-        .then(function (data) {
-            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
+        .then(function(weatherData) {
+            displayWeather(weatherData);
         })
-        .then(function () {
-            displayWeather();
+        .catch(function(){
+            console.log("Unable to fetch weather data");
         });
 }
 
-*/
-
-// Visar displayen för användargränssnittet. Ikonerna är länkade här.
-function displayWeather() {
-    iconElement.innerHTML = `<img src="/static/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    descElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
-}
-
-// Celsius till Fathrenheit omvandlare
-function celsiusToFahrenheit(temperature) {
-    return (temperature * 9 / 5) + 32;
-}
-
-// När man klickar på temperaturen så bytas celsius till fahrenheit och vice versa
-tempElement.addEventListener("click", function () {
-    if (weather.temperature.value === undefined) return;
-
-    if (weather.temperature.unit == "celsius") {
-        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
-        fahrenheit = Math.floor(fahrenheit);
-
-        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
-        weather.temperature.unit = "fahrenheit";
-    } else {
-        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-        weather.temperature.unit = "celsius"
+function initializeWeatherWidget() {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            getCoordinates, error
+        );
     }
-});
+}
 
+window.addEventListener("load", initializeWeatherWidget);
