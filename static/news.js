7984d7cf453d5fc30dbff64b26c8c1a9511373a9
karlstamporoski <60219096+karlstamporoski@users.noreply.github.com>
2020-05-15 18:45:45 +0200

    Add files via upload

---
+++ static/news.js
@@ -0,0 +1,41 @@
+// rss source
+const RSS_URL = `https://rss.aftonbladet.se/rss2/small/pages/sections/nyheter/`;
+
+
+$.ajax(RSS_URL, {
+      accepts: {
+        xml: "application/rss+xml"
+      },
+      dataType: "xml",
+      success: function (data) {
+
+        //creates a template and target elements
+        $(data)
+          .find("item").slice(-5)
+          .each(function () {
+            counter = 5
+            const el = $(this);
+
+            const template = `
+          <News>
+            <rubrik>
+              <a href="${el
+                .find("link")
+                .text()}" target="_blank" rel="noopener">
+                ${el.find("title").text()}
+              </a>
+            </rubrik>
+            <p>
+                <a href="${el
+                    .find("link")
+                    .text()}" target="_blank" rel="noopener">
+                    ${el.find("description").text()}
+                </a>
+            </p>
+          </News>
+        `;
+            // insert text as HTML
+            document.body.insertAdjacentHTML("beforeend", template);
+          });
+      }
+});
