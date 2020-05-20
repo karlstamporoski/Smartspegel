abcb6c382edc99c8e114d46d22e7b7f1cf58c621
Azii4 <62041963+Azii4@users.noreply.github.com>
2020-05-20 15:52:31 +0200

    beautifier

--- static/moveit.js
+++ static/moveit.js
@@ -5,7 +5,7 @@ var lastDragged;
 
 // Create Draggables with the hitTest()
 Draggable.create(droppables, {
-  bounds: container,
+	bounds: container,
 	onDragStart: function(e) {
 		$(this.target).addClass("highlight");
 	},
@@ -27,60 +27,59 @@ Draggable.create(droppables, {
 		$(this.target).removeClass("highlight");
 		while (--i > -1) {
 			if (this.hitTest(droppables[i], overlapThreshold)) {
-                          moveOtherDraggable(this.target, droppables[i], event);
+				moveOtherDraggable(this.target, droppables[i], event);
 			}
 		}
 	}
 });
 
-function moveUntillOutside(dragged, dropped, overlapThreshold, pos){
-  if (Draggable.hitTest(dragged, dropped, overlapThreshold)){
-    TweenMax.to(dragged, 0.00001, pos);
-  }
+function moveUntillOutside(dragged, dropped, overlapThreshold, pos) {
+	if (Draggable.hitTest(dragged, dropped, overlapThreshold)) {
+		TweenMax.to(dragged, 0.00001, pos);
+	}
 }
 
 function moveOtherDraggable(dragged, dropped, event) {
-  var w = $(dropped).width();
-  var h = $(dropped).height();
-  var offset = $(dropped).offset();
+	var w = $(dropped).width();
+	var h = $(dropped).height();
+	var offset = $(dropped).offset();
 
-  var x = (event.pageX - offset.left - (w / 2)) * (w > h ? (h / w) : 1);
-  var y = (event.pageY - offset.top - (h / 2)) * (h > w ? (w / h) : 1);
-  var direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180) / 90) + 3) % 4;
+	var x = (event.pageX - offset.left - (w / 2)) * (w > h ? (h / w) : 1);
+	var y = (event.pageY - offset.top - (h / 2)) * (h > w ? (w / h) : 1);
+	var direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180) / 90) + 3) % 4;
 
-  let pos = {
-   
+	let pos = {
 
-  };
+	};
 
-  switch (direction) {
-    // Top
-    case 0:
-      pos.top = "-=20px";
-      break;
-    // Right
-    case 1:
-      pos.left = "+=20px";
-      break;
-    // Bottom
-    case 2:
-      pos.top = "+=20px";
-      break;
-    // Left
-    case 3:
-      pos.left = "-=20px";
-      break;
-  }
+	switch (direction) {
+		// Top
+		case 0:
+			pos.top = "-=20px";
+			break;
+			// Right
+		case 1:
+			pos.left = "+=20px";
+			break;
+			// Bottom
+		case 2:
+			pos.top = "+=20px";
+			break;
+			// Left
+		case 3:
+			pos.left = "-=20px";
+			break;
+	}
 
-pos.onComplete = function(){
-  moveUntillOutside(dragged, dropped, overlapThreshold, pos);
-}
-  // TODO: change the position from +/- 30px to instead check where
-  //       the `dropped` element is positioned and place the `dragged`
-  //       element outside of this element (with an additional, perhaps,
-  //       +/- 30px like before).
-  //	   Or re check if the 'dragged' element is still in collision with the
-  //	   element and re run the logic of the kollision function 
+	pos.onComplete = function() {
+		moveUntillOutside(dragged, dropped, overlapThreshold, pos);
+	}
+	// TODO: change the position from +/- 30px to instead check where
+	//       the `dropped` element is positioned and place the `dragged`
+	//       element outside of this element (with an additional, perhaps,
+	//       +/- 30px like before).
+	//	   Or re check if the 'dragged' element is still in collision with the
+	//	   element and re run the logic of the kollision function 
 
-  TweenMax.to(dragged, 0.00001, pos);
+	TweenMax.to(dragged, 0.00001, pos);
 }
