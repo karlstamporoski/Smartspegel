b45eb8d5065972570c9c6a6c6967d99ee04eb322
karlstamporoski <karl_stamporoski@hotmail.com>
2020-05-22 19:30:24 +0200

    News-widget added style and scroll function

--- static/news.js
+++ static/news.js
@@ -3,38 +3,40 @@ const RSS_URL = `https://rss.aftonbladet.se/rss2/small/pages/sections/nyheter/`;
 
 
 $.ajax(RSS_URL, {
-      accepts: {
-        xml: "application/rss+xml"
-      },
-      dataType: "xml",
-      success: function (data) {
+  accepts: {
+    xml: "application/rss+xml"
+  },
+  dataType: "xml",
+  success: function (data) {
 
-        //creates a template and target elements
-        $(data)
-          .find("item").slice(-7)
-          .each(function () {
-            counter = 5
-            const el = $(this);
+    // creates a template and target elements
+    $(data)
+      .find("item")
+      .slice(-5)
+      .each(function Item() {
+        counter = 5
+        const el = $(this);
 
-            const template = `
-            <header>
-              <a ="${el
-                .find("link")
-                .text()}" target="_blank" rel="noopener">
-                ${el.find("title").text()}
-              </a>
-            </header>
-            <content>
-              <a ="${el
+        const template = `
+        <wraper>
+              <header>
+                <a ="${el
                   .find("link")
                   .text()}" target="_blank" rel="noopener">
-                  ${el.find("description").text()
-                .slice(2)} 
-              </a>
-            </content>
-      `;
-      // insert text as HTML
-      document.querySelector(".NewsFeed").insertAdjacentHTML("beforeend", template);
-    });
+                  ${el.find("title").text()}
+                </a>
+              </header>
+                <p ="${el
+                    .find("link")
+                    .text()}" target="_blank" rel="noopener">
+                    ${el.find("description").text()
+                  .slice(2)} 
+                </p>
+              </wraper>
+        `;
+
+        // insert text as HTML
+        document.querySelector(".NewsFeed").insertAdjacentHTML("beforeend", template);
+      });
 }
 });
