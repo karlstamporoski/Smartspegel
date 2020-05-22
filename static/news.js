6163ef71480a13782091251727bf7268fdd3ea9e
karlstamporoski <karl_stamporoski@hotmail.com>
2020-05-22 19:00:10 +0200

    added style and scroll

--- static/news.js
+++ static/news.js
@@ -3,38 +3,40 @@ const RSS_URL = `https://rss.aftonbladet.se/rss2/small/pages/sections/nyheter/`;
 
 
 $.ajax(RSS_URL, {
-      accepts: {
-        xml: "application/rss+xml"
-      },
-      dataType: "xml",
-      success: function (data) {
+  accepts: {
+    xml: "application/rss+xml"
+  },
+  dataType: "xml",
+  success: function (data) {
 
-        //creates a template and target elements
-        $(data)
-          .find("item")
-          .each(function () {
-            const el = $(this);
+    // creates a template and target elements
+    $(data)
+      .find("item")
+      .slice(-5)
+      .each(function Item() {
+        counter = 5
+        const el = $(this);
 
-            const template = `
-          <News>
-            <rubrik>
-              <a href="${el
-                .find("link")
-                .text()}" target="_blank" rel="noopener">
-                ${el.find("title").text()}
-              </a>
-            </rubrik>
-            <p>
-                <a href="${el
+        const template = `
+        <wraper>
+              <header>
+                <a ="${el
+                  .find("link")
+                  .text()}" target="_blank" rel="noopener">
+                  ${el.find("title").text()}
+                </a>
+              </header>
+                <p ="${el
                     .find("link")
                     .text()}" target="_blank" rel="noopener">
-                    ${el.find("description").text()}
-                </a>
-            </p>
-          </News>
+                    ${el.find("description").text()
+                  .slice(2)} 
+                </p>
+              </wraper>
         `;
-            // insert text as HTML
-            document.getElementById('News').innerHTML = template;
-          });
-      }
+
+        // insert text as HTML
+        document.querySelector(".NewsFeed").insertAdjacentHTML("beforeend", template);
+      });
+}
 });
